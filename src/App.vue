d61952b90bbc264de5320db24e495b87b39eabc7
Kenmuraki5 <89627339+Kenmuraki5@users.noreply.github.com>
2023-02-26 14:53:11 +0700

    update navbar&carousel

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
+  <MainNavbar/>
+  <EventBar></EventBar>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App"/> 
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MainNavbar from './components/Navbar.vue'
+import EventBar from './components/CarouselBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MainNavbar,
+    EventBar
   }
-}
+} 
 </script>
 
 <style>
@@ -21,6 +27,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
