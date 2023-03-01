44f5d9e47b5230d6b40f68de625ce33a16894d30
Kenmuraki5 <89627339+Kenmuraki5@users.noreply.github.com>
2023-03-01 19:41:19 +0700

    change path file

--- src/components/MainitemList.vue
+++ src/components/MainitemList.vue
@@ -32,8 +32,8 @@
     </div>
   </div>
 </template>
-
 <script>
+import Manga from "../data/book.js"
 export default {
   name: 'MainitemList',
   props:{
@@ -42,53 +42,7 @@ export default {
   data(){
     return{
       amount: 1,
-      products:[
-        {
-          id: 1,
-          name: 'Jujutsu Kaisen vol 22',
-          href: '#',
-          imageSrc: 'https://preview.redd.it/volume-22-cover-v0-225990wokxfa1.jpg?width=640&crop=smart&auto=webp&s=11ae726a0a173fe1680f1e6937300d72c1c0c4c8',
-          imageAlt: "Front of men's Basic Tee in black.",
-          price: '$90',
-          color: 'MANGA',
-          quantity:0
-
-          
-        },
-        {
-          id: 2,
-          name: 'Lust Guess vol 6',
-          href: '#',
-          imageSrc: 'https://kbimages1-a.akamaihd.net/7ea5a327-af04-4e18-bfb3-635dbab0ed37/1200/1200/False/lust-geass-vol-6.jpg',
-          imageAlt: "Front of men's Basic Tee in black.",
-          price: '$90',
-          color: 'MANGA',
-          quantity:0
-
-        },
-        {
-          id: 3,
-          name: 'OSHI NO KO vol 10',
-          href: '#',
-          imageSrc: 'https://cdn.discordapp.com/attachments/880831085431390301/1079329874747076638/Kubo-san_Volume_11.webp',
-          imageAlt: "Front of men's Basic Tee in MANGA.",
-          price: '$90',
-          color: 'MANGA',
-          quantity:0
-
-        },
-        {
-          id: 4,
-          name: 'KAIJYU NO.8 vol 10',
-          href: '#',
-          imageSrc: 'https://pbs.twimg.com/media/FoAs23dXgAEzYqC?format=jpg&name=4096x4096',
-          imageAlt: "Front of men's Basic Tee in MANGA.",
-          price: '$90',
-          color: 'MANGA',
-          quantity:0
-
-        },
-      ]
+      products: Manga
     }
   }
 }
