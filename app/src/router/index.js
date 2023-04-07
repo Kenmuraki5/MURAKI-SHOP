15dfd797f7fe18c9a7f748fedfeae0b83f5a4bba
Kenmuraki5 <89627339+Kenmuraki5@users.noreply.github.com>
2023-04-07 15:01:21 +0700

    update

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -3,13 +3,17 @@ import Homepage from '../pages/Home.vue'
 import AllBook from '../pages/AllBook.vue'
 import Mangapage from '../pages/Manga.vue'
 import Novelpage from '../pages/Novel.vue'
+import Loginpage from '../pages/LoginPage.vue'
+import Registerpage from '../pages/RegisterPage.vue'
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path:'/', name:"Home", component: Homepage },
         {path:'/AllBook', name:"AllBook", component: AllBook },
         {path:'/Manga', name:"Manga", component: Mangapage },
-        {path:'/Novel', name:"Novel", component: Novelpage }
+        {path:'/Novel', name:"Novel", component: Novelpage },
+        {path:'/LoginPage', name:"Login", component: Loginpage },
+        {path:'/RegisterPage', name:"Register", component: Registerpage }
     ]
 })
 
