4090f84f87d8ec5f0570adb6a088b9a0c083ed65
Kenmuraki5 <89627339+Kenmuraki5@users.noreply.github.com>
2023-04-10 00:54:10 +0700

    update

--- app/src/pages/LoginPage.vue
+++ app/src/pages/LoginPage.vue
@@ -1,5 +1,5 @@
 <template>
-    <MainNavbar/>
+    <MainNavbar :add="addtoCart" :totalCart="totalCart" :totalPrice="totalPrice" :remove="removefromCart" :cart="cart" />
     <section class="min-h-screen flex flex-col">
         <nav>
             <div class="px-4 py-8 text-center text-2xl">
@@ -8,27 +8,26 @@
         </nav>
         <div class="flex flex-1 items-center justify-center">
             <div class="rounded-lg sm:border-2 px-4 lg:px-24 py-16 lg:max-w-xl sm:max-w-md w-full text-center">
-                <form class="text-center">
-                    <h1 class="font-bold tracking-wider text-3xl mb-8 w-full text-gray-600">
-                        Sign in
-                    </h1>
-                    <div class="py-2 text-left">
-                        <input type="email"
-                            class="bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700 "
-                            placeholder="Email" />
-                    </div>
-                    <div class="py-2 text-left">
-                        <input type="password"
-                            class="bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700 "
-                            placeholder="Password" />
-                    </div>
-                    <div class="py-2">
-                        <button type="submit"
-                            class="border-2 border-gray-100 focus:outline-none bg-purple-600 text-white font-bold tracking-wider block w-full p-2 rounded-lg focus:border-gray-700 hover:bg-purple-700">
-                            Sign In
-                        </button>
-                    </div>
-                </form>
+                <h1 class="font-bold tracking-wider text-3xl mb-8 w-full text-gray-600">
+                    Sign in
+                </h1>
+                <div class="py-2 text-left">
+                    <input type="text" name="username" v-model="username"
+                        class="bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700 "
+                        placeholder="Email" />
+                </div>
+                <div class="py-2 text-left">
+                    <input type="password" name="password" v-model="password"
+                        class="bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700 "
+                        placeholder="Password" />
+                </div>
+                <div class="py-2">
+                    <button type="submit" @click="submit()"
+                        class="border-2 border-gray-100 focus:outline-none bg-purple-600 text-white font-bold tracking-wider block w-full p-2 rounded-lg focus:border-gray-700 hover:bg-purple-700">
+                        Sign In
+                    </button>
+                </div>
+
                 <div class="text-center">
                     <a href="#" class="hover:underline">Forgot password?</a>
                 </div>
@@ -46,13 +45,66 @@
 </template>
 
 <script>
+import axios from 'axios';
 import MainNavbar from '../components/Navbar.vue'
-    export default{
-        components: {
-            MainNavbar,
-        },
-        data(){
-            
+export default {
+    components: {
+        MainNavbar,
+    },
+    data() {
+        return {
+            cart: [],
+            username: "johndoe",
+            password: "password123"
         }
+    },
+    methods: {
+        addtoCart(value) {
+            for (let i = 0; i < this.cart.length; i++) {
+                if (this.cart[i].isbn == value.isbn) {
+                    this.cart[i].quantity++
+                    return localStorage.setItem("cart", JSON.stringify(this.cart))
+                }
+            }
+            Object.assign(value, { quantity: 0 });
+            value.quantity = 1
+            this.cart.push(value)
+            localStorage.setItem("cart", JSON.stringify(this.cart))
+        },
+        removefromCart(value) {
+            this.cart[this.cart.indexOf(value)].quantity-- == 1 ? this.cart.splice(this.cart.indexOf(value), 1) : 1
+            // this.cart.splice(this.cart.indexOf(value), 1)
+            localStorage.setItem("cart", JSON.stringify(this.cart))
+        },
+        submit() {
+            var formData = new FormData();
+            formData.append("username", this.username);
+            formData.append("password", this.password)
+            axios.post('http://localhost:3000/signin', formData, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then((response) => {
+                console.log(response.data)
+                this.$router.push({ name: "HomePage" })
+            }).catch(() => {
+                alert("Invalid email or password")
+            });
+        },
+    },
+    computed: {
+        totalPrice() {
+            return this.cart.reduce((total, item) => total + (parseInt(item.book_price)) * item.quantity, 0)
+        },
+        totalCart() {
+            return this.cart.reduce((total, item) => total + item.quantity, 0)
+        }
+    },
+    created() {
+        this.cart = JSON.parse(localStorage.cart == undefined ? "[]" : localStorage.cart)
+        axios.get(`http://localhost:3000/AllBook`)
+            .then(res => this.products = res.data)
+            .catch(err => console.log(err))
     }
+}
 </script>
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -12,5 +12,21 @@ router.get("/", async function (req, res, next) {
     return next(err)
   }
 });
-
+router.post("/signin", async function (req, res, next) {
+  try {
+    let result = await pool.query("SELECT * FROM Customer where c_username = ? and c_password = ?", [req.body.username, req.body.password]);
+    let result2 = await pool.query("SELECT * FROM Admin where a_username = ? and a_password = ?", [req.body.username, req.body.password]);
+    if(result[0].length != 0){
+      res.redirect("/")
+    }
+    if(result2[0].length != 0){
+      res.redirect("/")
+    }
+    else{
+      throw new Error("eee")
+    }
+  } catch (err) {
+    next(err)
+  }
+});
 exports.router = router;
