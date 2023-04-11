aaf15f6989369afd625b91e09b4a744998f81e3b
Kenmuraki5 <89627339+Kenmuraki5@users.noreply.github.com>
2023-04-11 19:15:39 +0700

    update

--- app/src/pages/CheckOut.vue
+++ app/src/pages/CheckOut.vue
@@ -1,14 +1,49 @@
 <template>
     <MainNavbar :add="addtoCart" :totalCart="totalCart" :totalPrice="totalPrice" :remove="removefromCart" :cart="cart" />
-    <div class="container px-6" style="margin-top: 100px;">
+    <div class="container px-6 py-20">
         <div class="row">
             <div class="col-md-8">
                 <div class="">
                     <h1>Product list</h1>
                 </div>
                 <!-- ส่วนของ product-->
-                <div class="row justify-content-center mb-3 mt-3" id="product">
-
+                <div class="row justify-content-center mb-3 mt-3">
+                    <div class="col-md-12 col-xl-12 my-3" v-for="product in cart" :key="product.book_isbn">
+                        <div class="card shadow-0 border rounded-3">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-12 col-lg-3 col-xl-3 mb-4 mb-lg-0">
+                                        <div class="bg-image hover-zoom ripple rounded ripple-surface">
+                                            <img :src="product.book_img" class="w-100" />
+                                            <a href="#!">
+                                                <div class="hover-overlay">
+                                                    <div class="mask" style="background-color: rgba(253, 253, 253, 0.15);">
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6 col-lg-6 col-xl-6">
+                                        <h5>{{ product.book_name }}</h5>
+                                        <h3 class="">
+                                            amount: ${{ product.quantity }}
+                                        </h3>
+                                        <h5 style="position:absolute; bottom:10px;">total price:
+                                            <span style="color:red;">${{ product.quantity * product.book_price }} ฿</span>
+                                        </h5>
+                                    </div>
+                                    <div class="col-md-12 col-lg-3 col-xl-3 border-sm-start-none border-start">
+                                        <div class="d-flex justify-content-between mt-4">
+                                            <h6 class="text-success">price</h6>
+                                            <div class="div">
+                                                <h4 style="text-align: right;">${{ product.book_price }} ฿</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="col-md-4 p-3">
@@ -27,31 +62,37 @@
                        </label>
                    </div> -->
                     <h5>Address</h5>
-                    <form method="POST">
+                    <form>
                         <div class="p-3 d-flex justify-content-center flex-column" id="delivery-address"></div>
                         <div class="form-group row mt-3">
-                            <label for="Phone" class="col-sm-2 col-form-label">Phone:</label>
-                            <div class="col-sm-10">
+                            <label for="Phone" class="col-sm-4 col-form-label">Phone:</label>
+                            <div class="col-sm-8">
                                 <input type="number" class="form-control" name="phone" placeholder="Phone number" required>
+                            </div><br><br><br>
+                            <label for="countries" class="col-sm-4 col-form-label text-sm text-gray-900 dark:text-white">ตัวเลือกการจัดส่ง</label>
+                            <div class="col-sm-8">
+                                <select id="countries"
+                                class="form-control" v-model="selected">
+                                    <option v-if="!selected" value="">Select Shipping</option>
+                                    <option :value="val" v-for="val in shipping" :key="val.shipping_id">{{val.shipping_name}}</option>
+                                </select>
+                            </div>
+                            <br><br><br>
+                            <label></label>
+                        </div>
+                        <hr>
+                        <h3 style="text-align:center;" class="py-4">ข้อมูลการชำระเงิน</h3>
+                        <!-- ส่วนของ ราคารวม -->
+                        <div class="grid px-4 my-4">
+                            <div class="flex justify-between">
+                                <span>ราคารวม</span>
+                                <span class="text-rose-800">{{ totalPrice }} ฿</span>
+                            </div>
+                            <div class="flex justify-between">
+                                <span>ค่าจัดส่ง</span>
+                                <span class="text-rose-800">{{ selected == "" ? "Please select shipping" : selected.cost }} ฿</span>
                             </div>
                         </div>
-                        <hr>
-                        <div id="d" style="display: none;">
-
-                        </div>
-                        <h3 style="text-align:center;">Summary</h3>
-                        <!-- ส่วนของ summart -->
-                        <div class="p-3 justify-content-between" id="summary"></div>
-                        <hr>
-                        <!-- ส่วนของ ราคารวม -->
-                        <div id="totalprice"></div>
-                        <div id="b" style="display: none;">
-
-                        </div>
-                        <hr>
-                        <h6 style="text-align: center; color: rgb(229, 48, 78);">Now the system only has cash on delivery.
-                        </h6>
-                        <hr>
                         <input type="submit" name="SUBMIT" value="Payment" class="btn btn-warning" style="width:100%;"><br>
                     </form>
                 </div>
@@ -63,6 +104,7 @@
 <script>
 import MainNavbar from '../components/Navbar.vue'
 import MainFooter from '../components/MainFooter.vue'
+import axios from 'axios'
 export default {
     components: {
         MainFooter,
@@ -70,7 +112,9 @@ export default {
     },
     data() {
         return {
-
+            cart: [],
+            shipping:null,
+            selected:""
         }
     },
     methods: {
@@ -100,8 +144,17 @@ export default {
             return this.cart.reduce((total, item) => total + item.quantity, 0)
         }
     },
-    created() {
-    this.cart = JSON.parse(localStorage.cart == undefined ? "[]" : localStorage.cart)
+    async created() {
+        try {
+            this.cart = JSON.parse(localStorage.cart == undefined ? "[]" : localStorage.cart);
+            const res = await axios.get("http://localhost:3000/CheckOut")
+            this.shipping = res.data
+            const res1 = await axios.get("http://localhost:3000/")
+            // const result = this.cart.filter(val => res1.data.find(val2 => val.isbn == val2.isbn))
+            // console.log(result)
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
 </script>
