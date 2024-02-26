472a848d7547b4096f0de08c73e4bc563d32c718
kenmuraki5 <89627339+Kenmuraki5@users.noreply.github.com>
2024-02-27 00:04:22 +0700

    fix bug --deployment

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: '192.168.1.35',
+  host: '34.125.149.37',
   user: 'root',
   password: 'root',
   database: 'MURAKI_COMIC2',
